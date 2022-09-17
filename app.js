dedc990de9723eb47602d7a1a90adea9cbd40d37
hafsa <hfsyln93@gmail.com>
2022-09-17 23:52:21 +0300

    edit

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".sourinbutton");
+const btn1 = document.querySelector(".plusinbutton");
+const miktar = document.querySelector(".miktar");
+const miktar1 = document.querySelector(".miktar1");
+const miktar2 = document.querySelector(".miktar2");
+
+
+const count = miktar.value;
+const count1 = miktar1.value;
+const count2 = miktar2.value;
+consol.log(count)
+
+btn.addEventListener("click", () => {
+
+})
+
+btn.addEventListener("click", () => {
+
+})
