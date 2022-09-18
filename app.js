00d9673aab3db89ff4a878b61af630ba02a2a4c7
hafsa <hfsyln93@gmail.com>
2022-09-18 15:22:34 +0300

    fınısh

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const counters = document.querySelectorAll(".buttons");
 const plusinbutton = document.querySelectorAll(".plusinbutton");
 let sourinbutton = document.querySelectorAll(".sourinbutton");
 let spaan = document.querySelectorAll("span");
-let sum = 0;
 const total1 = document.querySelectorAll(".total1");
-const shippingtotal = document.querySelector(".shipping")
-const tax = document.querySelector(".tax")
+const shippingtotal = document.querySelector(".shipping");
+const tax = document.querySelector(".tax");
+const totalall = document.querySelector(".alltotal");
+let sum = 0;
+let shipping = 15;
 
 
-tax.textContent = (sum.toFixed(2) *0.18) + "$"
 shippingtotal.textContent = (sum.toFixed(2) + "$")
+tax.textContent == (sum *0.18).toFixed(2) + "$"
+totalall.textContent = ((sum *0.18) + sum + shipping).toFixed(2) + "$"
    
 
 plusinbutton.forEach((item)=>{
@@ -25,17 +28,17 @@ plusinbutton.forEach((item)=>{
             console.log(shippingtotal.textContent)
         })
         tax.textContent = (sum *0.18).toFixed(2) + "$"
-    
+        totalall.textContent = ((sum *0.18) + sum + shipping).toFixed(2) + "$"
     }) 
-
 
 })
 
 
 sourinbutton.forEach((item)=> {
     item.addEventListener("click", () => {
+        if (item.nextElementSibling.textContent > 0) {
         --item.nextElementSibling.textContent ;
-    
+        }
     item.parentElement.nextElementSibling.children[0].textContent =
     (item.nextElementSibling.textContent * item.parentElement.previousElementSibling.children[0].textContent).toFixed(2);
 
@@ -46,6 +49,7 @@ sourinbutton.forEach((item)=> {
         console.log(shippingtotal.textContent)
     })
     tax.textContent = (sum *0.18).toFixed(2) + "$"
+    totalall.textContent = ((sum *0.18) + sum + shipping).toFixed(2) + "$"
 
 
     })
