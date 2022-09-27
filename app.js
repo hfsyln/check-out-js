360820a6611346a7831c990dafb72f69d37180e1
hafsa <hfsyln93@gmail.com>
2022-09-27 23:37:21 +0300

    different way add

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const counters = document.querySelectorAll(".buttons");
 const plusinbutton = document.querySelectorAll(".plusinbutton");
 const minusinbutton = document.querySelectorAll(".minusinbutton");
-const spaan = document.querySelectorAll("span");
-const total1 = document.querySelectorAll(".total1");
 const shippingtotal = document.querySelector(".shipping");
 const tax = document.querySelector(".tax");
 const totalall = document.querySelector(".alltotal");
 const remove = document.querySelectorAll(".remove");
+//const shippingFree = 300
 let sum = 146.96;
-let shipping = 15;
+//const shipping = 15;
+//const taxRate = 0.18
+
+//sayfa yüklenince sabit değerleri locale at
+//aynı bilgilerin başka sayfalarda tanımlamadan kulanımını sağlar
+//sayfalar arası veri aktarımı
+window.addEventListener("load", ()=>{
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shipping", shipping)
+    localStorage.setItem("shippingFree", shippingFree)
+
+    sessionStorage.setItem("taxRate", taxRate);
+    sessionStorage.setItem("shipping", shipping)
+    sessionStorage.setItem("shippingFree", shippingFree)
+
+    //session browser kapanınca gider local kalır
+})
 
 
 shippingtotal.textContent = (sum.toFixed(2) + "$")
@@ -64,8 +78,7 @@ minusinbutton.forEach((item)=> {
 
 remove.forEach((item) =>{
     item.addEventListener("click",() =>{
-        i
-        tem.parentElement.parentElement.remove()
+        item.parentElement.parentElement.remove()
         
     
 
@@ -95,6 +108,69 @@ remove.forEach((item) =>{
 })
 
 
+////////////////////////////////////////////! 
+/*
+////////////////////*diğer yöntem //derste çözülen
+//yukarıdaki tanımlamalara gerek kalmadı
+calculateCartPrice() //sayfanın başına bunu taşırsak açılıştaki 1 er elemanın değerlerini toplar ve altta 0 0 0 değil doğru değerler yazar
+const allBag= document.querySelector(".shippingbox")
 
+allBag.addEventListener("click", (e)=>{
+    console.log(e.target)
 
+    if(e.target.className == "minusinbutton"){
+        if(e.target.nextElementSibling.innerText > 1){
+            e.target.nextElementSibling.innerText--   //dört işlem string ile ortadaki oparetör sayesinde sayı gibi işlem yapılır
+            calculateProductPrice(e.target);
+            calculateCartPrice();
+        } else {
+                if(confirm("product will be remove")){ //confirm iki seçim sunar ok if i çalıştırır cancel else durumunu
+                    //remove
+                    e.target.parentElement.parentElement.parentElement.remove()
+                    calculateCartPrice();
+                }
+            }
 
+    }
+    else if (e.target.className == "plusinbutton") {
+        e.target.previousElementSibling.innerText++;
+        calculateProductPrice(e.target);
+        calculateCartPrice();
+    }
+    else if(e.target.className == "remove"){
+        e.target.parentElement.previousElementSibling.previousElementSibling.remove()
+        calculateCartPrice();
+    }
+
+    else {
+        console.log("başka elemente tıklayınız")
+    }
+
+})
+//yukardaki her bir ürün için 
+const calculateProductPrice = (clickedBtn) => {
+    console.log(clickedBtn);
+    const prodactInfoDiv = clickedBtn.parentElement.parentElement; //kapsayıcı div ulaşmak için
+    const price = prodactInfoDiv.children[1][0].textContent; //fiyat
+    const quantitiy = prodactInfoDiv.querySelector( ".miktar").innerText //miktar
+    const prodactTotalDiv =  prodactInfoDiv.querySelector("p .total1");
+    prodactTotalDiv.innerHTML = (price * quantitiy).toFixed(2) //dönüşüme gerek yok number oldu
+
+}
+///alttaki genel toplam için
+const calculateCartPrice = () => {
+    const tümToplam = document.querySelectorAll(".total1")
+    //const tümToplam = document.getElementsByClassName(".total1")  // [...docum......] baştaki 3 nokta ile array e çevir
+    let subTotal =  0
+    tümToplam.forEach((div)=>{
+        subTotal += parseFloat(div.innerHTML) //işlem yapılır
+    });
+    const taxPrice = subTotal * localStorage.getItem("taxRace"); //yukarda yorum satırında olsaydı localden çekmek için
+    const shippingPrice = parseFloat((subTotal > 0 && subTotal > localStorage.getItem("shippingFree")? localStorage.getItem("shipping") : 0)) //parse ile sayı oldu
+
+    document.querySelector(".tax").innerHTML = taxPrice.toFixed(2);
+    document.querySelector(".alltotal").innerHTML = tümToplam.toFixed(2);
+    document.querySelector(".ship").innerHTML = (tümToplam + taxPrice + shippingPrice).toFixed(2)    document.querySelector(".shipping").innerHTML = shippingPrice
+
+}
+*/
